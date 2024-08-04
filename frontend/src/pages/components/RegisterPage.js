86252ba021ebb112c6d456a05055654d6052f427
noufa03 <salim2002621@gmail.com>
2024-08-04 19:56:17 +0530

    loginpage aand register page

---
+++ frontend/src/pages/components/RegisterPage.js
@@ -0,0 +1,125 @@
+import * as React from 'react';
+import { useState } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+
+
+const defaultTheme = createTheme();
+
+const SignUp=() =>{
+  const [email, setemail] = useState("")
+  const [password, setpassword] = useState("")
+  const [confirmPassword,setconfirmpassword]=useState("")
+  const [Fullname,setfullname]=useState("")
+  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(email != "" && password !=""){
+        alert(" Registration Successful")
+    }else{
+      alert("please fill out the form first")
+    }
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1,width:'400px' }}>
+          <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="fullname"
+              label="Full Name"
+              name="name"
+              autoComplete="name"
+              autoFocus
+              value={Fullname}
+              onChange={(e)=>setfullname(e.target.value)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+            
+              value={email}
+              onChange={(e)=>setemail(e.target.value)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={(e)=>setpassword(e.target.value)}
+            />
+             <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="confirmpassword"
+              label=" Confirm The Password"
+              type="password"
+              id="password"
+        
+              value={confirmPassword}
+              onChange={(e)=>setconfirmpassword(e.target.value)}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign up
+            </Button>
+          
+          </Box>
+        </Box>
+       
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default SignUp

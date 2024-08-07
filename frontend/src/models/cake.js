bc4e7238cfaad1f57a567982c9a575c481bb7910
Lakshith Nishshanke <37945916+lakshith-403@users.noreply.github.com>
2024-08-07 14:48:03 +0530

    Fetch cakes from backend API
    Fetch menu items from backend API instead of using hardcoded data.

---
+++ frontend/src/models/cake.js
@@ -0,0 +1,11 @@
+class Cake {
+  constructor(name, description, price, image, category) {
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.image = image;
+    this.category = category;
+  }
+}
+
+export default Cake;

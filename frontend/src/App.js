a1728654d4052afb051e9f30e6206127a9609c37
lakshith <lakshith.k.nishshanke@gmail.com>
2024-08-11 22:10:39 +0530

    create a color theme for mui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,37 @@ import Home from './pages/home';
 import Menu from './pages/Menu';
 import Layout from './layout';
 import Cart from './pages/Cart';
+import { createTheme } from '@mui/material/styles';
+import {ThemeProvider} from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ff69b4', // Customize your primary color
+    },
+    secondary: {
+      main: '#9b2226', // Customize your secondary color
+    },
+  },
+  typography: {
+    fontFamily: 'Arial, sans-serif', // Customize your font family
+  },
+});
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="menu" element={<Menu />} />
-          <Route path="cart" element={<Cart />} />
-        </Route>
-      </Routes>
-    </Router>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="menu" element={<Menu />} />
+              <Route path="cart" element={<Cart />} />
+            </Route>
+          </Routes>
+        </Router>
+  </ThemeProvider>
+
   );
 }
 

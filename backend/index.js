c1643c7fae76d0cb695bb6140808947c91e6f9db
noufa03 <salim2002621@gmail.com>
2024-08-20 14:48:10 +0530

    backend start

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const cors = require('cors')
-
+const loginpage=require('./routes/login')
+const registerpage=require('./routes/register')
 const app = express()
 app.use(cors());
+//use the bodyparser for body in login.js
+app.use(bodyParser.json()); // <-- This is crucial
+app.use(bodyParser.urlencoded({ extended: true }));
 const port = 3001
 
 app.get('/', (req, res) => {
@@ -10,6 +15,21 @@ app.get('/', (req, res) => {
         message: "Hello World from Express API backend!"
     })
 })
+// Data
+const items = [
+    { id: 1, name: 'Item 1' },
+    { id: 2, name: 'Item 2' },
+    { id: 3, name: 'Item 3' },
+];
+
+// Route to get items
+app.get('/api/items', (req, res) => {
+    res.json(items);
+});
+//routes 
+//path name with api any name
+app.use('/api/login',loginpage)
+app.use('/api/registerpage',registerpage)
 
 app.listen(port, () => {
 console.log(`Example app listening on port ${port}`)

c0fefa0cd906059827cb60a013531373d831ff2d
lakshith <lakshith.k.nishshanke@gmail.com>
2024-09-08 16:54:19 +0530

    implement login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -31,7 +31,7 @@ router.post("/login", async (req, res) => {
     if (!isPasswordValid) {
       return res.status(400).send("Invalid email or password");
     }
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+    const token = jwt.sign({ userId: user._id }, "hehe", {
       expiresIn: "1h",
     });
     res.status(200).json({ token });
